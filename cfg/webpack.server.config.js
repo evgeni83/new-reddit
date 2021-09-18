eb028edbc601eb8d4c79a9f453b2f7b942098d3e
evgeni83 <evgeni.daniluk@yandex.ru>
2021-09-18 22:07:16 +0300

    onlyLocals issue in css-loader options fixed

--- cfg/webpack.server.config.js
+++ cfg/webpack.server.config.js
@@ -25,7 +25,8 @@ module.exports = {
                 options: {
                     modules: {
                         mode: 'local',
-                        localIdentName: '[name]__[local]--[hash:base64:5]'
+                        localIdentName: '[name]__[local]--[hash:base64:5]',
+                        exportOnlyLocals: true
                     },
                 }
             } ]
