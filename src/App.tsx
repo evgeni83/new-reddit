d3a0841f32c1d2e049ff9fdb79fbad876e5d1161
evgeni83 <evgeni.daniluk@yandex.ru>
2021-09-26 16:49:14 +0300

    Header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './main.global.css';
 import {hot} from "react-hot-loader/root";
+import {Layout} from "./shared/Layout";
 
 const App = () => {
     return (
-        <div>
+        <Layout>
             content2
-        </div>
+        </Layout>
     );
 };
 
---
+++ src/shared/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './header.css';
+
+export function Header() {
+  return (
+
+  );
+}
