cc892cd94499c6794fcf2ad1c4af1f7a76bbc839
evgeni83 <evgeni.daniluk@yandex.ru>
2021-11-15 16:13:15 +0300

    4.5

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './main.global.css';
 import {hot} from "react-hot-loader/root";
 import {Layout} from "./shared/Layout";
+import {Header} from "./shared/Header";
+import {Content} from "./shared/Content";
 
 const App = () => {
     return (
         <Layout>
-            content2
+            <Header/>
+            <Content>
+                CONTENT
+            </Content>
         </Layout>
     );
 };
